001f69323a8d07f83726f19b8439af04db277f9a
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-03-15 02:19:31 -0300

    day 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,15 @@
+use std::collections::HashSet;
+
+fn main() {
+    let data = aoc2018::input_parse(1);
+    println!("{}", data.iter().sum::<i32>());
+
+    // part two.
+    let data = data.iter().cycle();
+    let mut seen = HashSet::from([0]);
+    let mut frequencies = data.scan(0, |acc, &n| {
+        *acc += n;
+        Some(*acc)
+    });
+    println!("{}", frequencies.find(|&f| !seen.insert(f)).unwrap());
+}
