37a37f5945bf186555c61280db4d3edc3ce44ad3
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-03-17 02:20:23 -0300

    lines input handling

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ pub fn input(day: u8) -> String {
     fs::read_to_string(format!("inputs/input{}.txt", day)).unwrap()
 }
 
+pub fn input_lines(day: u8) -> Vec<String> {
+    input(day).lines().map(ToOwned::to_owned).collect()
+}
+
 pub fn input_parse<T: FromStr>(day: u8) -> Vec<T>
 where
     <T as FromStr>::Err: fmt::Debug,
