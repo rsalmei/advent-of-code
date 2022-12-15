7273d2da2de7cfb74e4581e4dec108d3a9a4739f
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-12-15 03:01:16 -0300

    day 3

---
+++ src/aoc2022/day3.rs
@@ -0,0 +1,40 @@
+use crate::Input;
+use std::collections::HashSet;
+
+pub fn run(input: Input) {
+    let data = input.as_lines();
+
+    let mut priority = Vec::with_capacity(53);
+    priority.push(b'!');
+    priority.extend(b'a'..=b'z');
+    priority.extend(b'A'..=b'Z');
+
+    // part one.
+    let items = data
+        .iter()
+        .map(|&ks| {
+            let (a, b) = ks.split_at(ks.len() / 2);
+            let a = a.as_bytes().iter().copied().collect::<HashSet<_>>();
+            let b = b.as_bytes().iter().copied().collect::<HashSet<_>>();
+            *a.intersection(&b).next().unwrap()
+        })
+        .map(|i| priority.iter().position(|&t| i == t).unwrap());
+    println!("{}", items.sum::<usize>());
+
+    // part two.
+    let items = data
+        .chunks(3)
+        .map(|g| {
+            let a = g[0].as_bytes().iter().copied().collect::<HashSet<_>>();
+            let b = g[1].as_bytes().iter().copied().collect::<HashSet<_>>();
+            let c = g[2].as_bytes().iter().copied().collect::<HashSet<_>>();
+            *a.intersection(&b)
+                .copied()
+                .collect::<HashSet<_>>()
+                .intersection(&c)
+                .next()
+                .unwrap()
+        })
+        .map(|i| priority.iter().position(|&t| i == t).unwrap());
+    println!("{}", items.sum::<usize>())
+}
--- src/aoc2022/mod.rs
+++ src/aoc2022/mod.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
+mod day3;
 
 use crate::Input;
 
-pub const RUN: &[fn(Input)] = &[day1::run, day2::run];
+pub const RUN: &[fn(Input)] = &[day1::run, day2::run, day3::run];
