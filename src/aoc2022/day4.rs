dadf59145c9354853166c7c645d2ab465b5ee997
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2022-12-15 22:52:48 -0300

    day 4

---
+++ src/aoc2022/day4.rs
@@ -0,0 +1,46 @@
+use crate::Input;
+use std::str::FromStr;
+
+struct Pair {
+    a: u8,
+    b: u8,
+}
+
+impl FromStr for Pair {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (a, b) = s.split_once('-').ok_or(())?;
+        Ok(Self {
+            a: a.parse().map_err(|_| ())?,
+            b: b.parse().map_err(|_| ())?,
+        })
+    }
+}
+
+impl Pair {
+    fn fully_contains(&self, other: &Self) -> bool {
+        (self.a <= other.a && self.b >= other.b) || (other.a <= self.a && other.b >= self.b)
+    }
+
+    fn overlap(&self, other: &Self) -> bool {
+        !(self.a > other.b || self.b < other.a)
+    }
+}
+
+pub fn run(input: Input) {
+    let data = input
+        .as_lines()
+        .iter()
+        .map(|&s| s.split_once(',').unwrap())
+        .map(|(s, t)| (s.parse::<Pair>().unwrap(), t.parse().unwrap()))
+        .collect::<Vec<_>>();
+
+    // part one.
+    let redundant = data.iter().filter(|(p, q)| p.fully_contains(q));
+    println!("{}", redundant.count());
+
+    // part two.
+    let overlap = data.iter().filter(|(p, q)| p.overlap(q));
+    println!("{}", overlap.count());
+}
--- src/aoc2022/mod.rs
+++ src/aoc2022/mod.rs
@@ -1,7 +1,8 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use crate::Input;
 
-pub const RUN: &[fn(Input)] = &[day1::run, day2::run, day3::run];
+pub const RUN: &[fn(Input)] = &[day1::run, day2::run, day3::run, day4::run];
