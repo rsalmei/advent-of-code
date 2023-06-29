4d5526d5c9859e9f4bebbc0516831ecace1826b0
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2023-06-29 01:00:56 -0300

    enable to run all challenges in a given year

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,38 @@ use std::collections::BTreeMap;
 
 #[derive(Debug, Copy, Clone, Parser)]
 struct Args {
-    year: u16,
-    day: u8,
+    year: Option<u16>,
+    day: Option<u8>,
 }
 
 fn main() {
     println!("My Advent of Code");
     println!("-----------------");
-    let Args { year, day } = Args::parse();
-    println!("year: {}  day: {}\n", year, day);
 
     let aoc = [(2018, aoc2018::DAYS), (2022, aoc2022::DAYS)]
         .into_iter()
         .map(|(y, r)| (y, (1..).zip(r).collect::<BTreeMap<_, _>>()))
         .collect::<BTreeMap<_, _>>();
-    match aoc.get(&year).and_then(|r| r.get(&day)) {
-        Some(run) => match Input::new(year, day) {
-            Ok(input) => run(input),
-            Err(err) => println!("Couldn't load input: {err}"),
+    let Args { year, day } = Args::parse();
+    let (year, day) = (year.unwrap_or_default(), day.unwrap_or_default());
+    match aoc.get(&year) {
+        Some(runs) => match runs.get(&day) {
+            Some(run) => {
+                println!("year: {year}  day: {day}\n");
+                run(Input::new(year, day).unwrap())
+            }
+            None => {
+                println!("year: {year}");
+                for (&day, run) in runs {
+                    println!("\nday: {day}");
+                    run(Input::new(year, day).unwrap())
+                }
+            }
         },
         None => {
-            println!("Year or day not found, available:");
+            println!("\nCalling with only the year runs all challenges in there.\nAvailable:");
             aoc.iter().for_each(|(y, r)| {
-                println!("{y}");
+                print!("* {y}\n  ");
                 (1..=r.len()).for_each(|d| print!(" {d}"));
                 println!();
             })
