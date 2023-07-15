323d6efdcb802aa8e80e3d9903cdb6e5855e778c
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2023-07-15 03:19:29 -0300

    greatly improve day 5/18

--- src/aoc2018/day5.rs
+++ src/aoc2018/day5.rs
@@ -1,48 +1,31 @@
 use crate::Input;
-use std::{iter, mem};
+use std::iter;
 
 pub fn run(input: Input) {
     let data = Vec::from(input.lines().next().unwrap());
 
     // part one.
-    fn react(w: &[u8], out: &mut Vec<u8>) {
-        w.iter()
-            .chain(iter::once(&0))
-            .fold(None, |acc: Option<u8>, &d| {
-                match acc {
-                    Some(a) if a.to_ascii_lowercase() != d.to_ascii_lowercase() => out.push(a),
-                    Some(a) if a.is_ascii_lowercase() ^ d.is_ascii_lowercase() => return None,
-                    Some(a) => out.push(a),
-                    None => {}
-                };
-                Some(d)
-            });
+    fn react_fully<'a>(w: impl Iterator<Item = &'a u8>, out: &mut Vec<u8>) -> usize {
+        out.clear();
+        w.chain(iter::once(&0)).fold(None, |acc: Option<u8>, &d| {
+            match acc {
+                Some(a) if a.abs_diff(d) == b'a' - b'A' => return out.pop(),
+                Some(a) => out.push(a),
+                None => {}
+            };
+            Some(d)
+        });
+        out.len()
     }
-    fn react_fully(mut w: Vec<u8>) -> usize {
-        let mut out = Vec::with_capacity(w.len());
-        loop {
-            react(&w, &mut out);
-            match w.len() == out.len() {
-                true => break out.len(),
-                false => {
-                    mem::swap(&mut w, &mut out);
-                    out.clear();
-                }
-            }
-        }
-    }
-    println!("{}", react_fully(data.clone()));
+    let mut out = Vec::with_capacity(data.len());
+    println!("{}", react_fully(data.iter(), &mut out));
 
     // part two.
     let min_len = (b'a'..=b'z')
         .map(|unit| {
             let unit_up = unit.to_ascii_uppercase();
-            let polymer = data
-                .iter()
-                .filter(|&&c| c != unit && c != unit_up)
-                .copied()
-                .collect();
-            react_fully(polymer)
+            let polymer = data.iter().filter(|&&c| c != unit && c != unit_up);
+            react_fully(polymer, &mut out)
         })
         .min();
     println!("{}", min_len.unwrap());
